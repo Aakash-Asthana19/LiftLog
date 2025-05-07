7df82de048dcc2f1697936c1487a0abd9c51b6c4
Aakash-Asthana19 <143640384+Aakash-Asthana19@users.noreply.github.com>
2025-05-07 18:26:09 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,17 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCYLGNvOtPHyXeQF6IRK2OXLQM9a2vtgNs",
-  authDomain: "eloquent-hold-452818-d3.firebaseapp.com",
-  projectId: "eloquent-hold-452818-d3",
-  storageBucket: "eloquent-hold-452818-d3.firebasestorage.app",
-  messagingSenderId: "842237190744",
-  appId: "1:842237190744:web:7516f11e03b87b1ae0e02d",
-  measurementId: "G-0L5TQ8JM9L"
-};
+
+
+// const firebaseConfig = {
+//   apiKey: "**",
+//   authDomain: "**",
+//   projectId: "**",
+//   storageBucket: "**",
+//   messagingSenderId: "**",
+//   appId: "**",
+//   measurementId: "**"
+// };
 
 //Initialize Firebase only if it hasn't been initialized yet
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
